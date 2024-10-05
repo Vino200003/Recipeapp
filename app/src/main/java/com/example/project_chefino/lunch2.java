768a0c643dddbc23f56fa4a4abeb9930e8996041
Vinoshika Makeswaran <153353810+Vino200003@users.noreply.github.com>
2024-10-05 20:05:52 +0530

    LUNCH PAGE

--- app/src/main/java/com/example/project_chefino/lunch2.java
+++ app/src/main/java/com/example/project_chefino/lunch2.java
@@ -24,9 +24,16 @@ public class lunch2 extends AppCompatActivity {
 
         // Sample data - Replace with data from your database
         recipeList = new ArrayList<>();
-        recipeList.add(new LunchRecipe("Grilled Cheese Sandwich", "A delicious grilled cheese sandwich.", R.drawable.grilled_cheese)); // Replace with your actual drawable resource
-        recipeList.add(new LunchRecipe("Caesar Salad", "Fresh Caesar salad with dressing.", R.drawable.caesar_salad)); // Replace with your actual drawable resource
-
+        recipeList.add(new LunchRecipe("Chicken curry", "A delicious grilled cheese sandwich.", R.drawable.chicken_curry)); // Replace with your actual drawable resource
+        recipeList.add(new LunchRecipe("Chicken Nasi Goreng", "Fresh Caesar salad with dressing.", R.drawable.chicken_nasi_goreng)); // Replace with your actual drawable resource
+        recipeList.add(new LunchRecipe("Chicken Biryani", "Fresh Caesar salad with dressing.", R.drawable.chickenbiryani));
+        recipeList.add(new LunchRecipe("Cholle Bhature", "Fresh Caesar salad with dressing.", R.drawable.cholle_bhature));
+        recipeList.add(new LunchRecipe("Curd Rice", "Fresh Caesar salad with dressing.", R.drawable.curd_rice));
+        recipeList.add(new LunchRecipe("Egg Curry", "Fresh Caesar salad with dressing.", R.drawable.egg_curry));
+        recipeList.add(new LunchRecipe("Lemon Rice", "Fresh Caesar salad with dressing.", R.drawable.lemon_rice));
+        recipeList.add(new LunchRecipe("Paneer Butter Masala", "Fresh Caesar salad with dressing.", R.drawable.paneer_butter_masala));
+        recipeList.add(new LunchRecipe("Vegetable Biryani", "Fresh Caesar salad with dressing.", R.drawable.vegetable_biryani));
+        recipeList.add(new LunchRecipe("Caesar Salad", "Fresh Caesar salad with dressing.", R.drawable.caesar_salad));
 
 
         lunchRecipeAdapter = new LunchRecipeAdapter(recipeList);
