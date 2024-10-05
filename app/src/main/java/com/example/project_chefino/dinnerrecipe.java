7bc5cd49e466f54d4b6ed2a1f7dc38f19552cd2d
zimam012 <zimamram@gmail.com>
2024-10-05 20:15:20 +0530

    add recipe

---
+++ app/src/main/java/com/example/project_chefino/dinnerrecipe.java
@@ -0,0 +1,29 @@
+package com.example.project_chefino;
+
+public class dinnerrecipe {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    // Constructor
+    public  dinnerrecipe(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for description
+    public String getDescription() {
+        return description;
+    }
+
+    // Getter for image resource ID
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+}
--- app/src/main/java/com/example/project_chefino/lunch1.java
+++
@@ -1,6 +0,0 @@
-package com.example.project_chefino;
-
-import android.app.Activity;
-
-public class lunch1 extends Activity {
-}
