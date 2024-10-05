46d079dccd277f3e4da4014f134fb3b8c35ab6b9
ASUS <thujathaponnuthurai@gmail.com>
2024-10-05 13:23:04 +0530

    uyfuyjggg

--- app/src/main/java/com/example/project_chefino/home.java
+++ app/src/main/java/com/example/project_chefino/home.java
@@ -3,10 +3,12 @@ package com.example.project_chefino;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class home extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,14 @@ public class home extends AppCompatActivity {
         Button breakfastButton = findViewById(R.id.button1);
         Button lunchButton = findViewById(R.id.button2);
 
+
         // Set onClickListener for lunchButton
         lunchButton.setOnClickListener(v -> {
             Intent intent = new Intent(home.this, lunch1.class);
             startActivity(intent);
         });
+
+
 
 
     }
