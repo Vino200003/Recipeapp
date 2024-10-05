ae3f082006780f165171f4ee5d0a4abb199d42ba
zimam012 <zimamram@gmail.com>
2024-10-05 12:40:16 +0530

    m1

--- app/src/main/java/com/example/project_chefino/home.java
+++ app/src/main/java/com/example/project_chefino/home.java
@@ -25,7 +25,7 @@ public class home extends AppCompatActivity {
         lunchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(home.this, lunch.class);
+                Intent intent = new Intent(home.this,lunch.class);
                 startActivity(intent);
 
             }
---
+++ app/src/main/java/com/example/project_chefino/lunch.java
@@ -0,0 +1,9 @@
+package com.example.project_chefino;
+
+import android.app.Activity;
+
+public class lunch extends Activity {
+
+
+
+}
